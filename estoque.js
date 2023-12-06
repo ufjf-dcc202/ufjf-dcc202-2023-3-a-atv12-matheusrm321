75fa777cb41719aa881959457446123b363d58a6
Matheus Ribeiro Menezes <matheusribeiro.menezes@estudante.ufjf.br>
2023-12-06 20:36:25 -0300

    criado funcao getEstoque() e estoque

---
+++ estoque.js
@@ -0,0 +1,13 @@
+
+let estoque = {
+
+    'joao': [ { 'tipo':"maca", 'qtd': 1 }, { 'tipo': "pera", 'qtd': 1 }, ], 
+    'maria': [ { 'tipo': "maca", 'qtd': 2 }, { 'tipo': "banana", 'qtd': 4 } ],
+
+};
+
+export function getEstoque()
+{
+    return structuredClone(estoque);
+}
+
