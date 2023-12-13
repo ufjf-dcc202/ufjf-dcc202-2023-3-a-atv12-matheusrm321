da5df46629cdca5a5e10948eec1b61558b854779
Matheus Ribeiro Menezes <matheusribeiro.menezes@estudante.ufjf.br>
2023-12-13 20:20:11 -0300

    Feito o atualizaTela() e preencheLsita

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function leFormulario(event)
 {
     event.preventDefault();
     const fruta = document.entrada.fruta.value;
-    const quantidade = document.entrada.quantidade.value;
+    const quantidade = Number(document.entrada.quantidade.value);
     const origem = document.entrada.origem.value;
     const destino = document.entrada.destino.value;
 
@@ -24,23 +24,20 @@ function leFormulario(event)
     function atualizaTela()
     {
         const estoque = getEstoque();
-        const joao = estoque['joao'];
-        for(let i = 0; i < joao.length;i++){
-            const monte = joao[i];
-            const eLi = document.createElement('li');
-            eLi.innerText = `${monte.tipo}: ${monte.qtd}`;
-            olJoao.append(eLi);
-        }
+        preencheListaPessoa(estoque['joao'], olJoao);
+        preencheListaPessoa(estoque['maria'], olMaria);
     }
 
-    function atualizaTela()
+
+    function preencheListaPessoa(pessoa, lista)
     {
-        const maria = estoque['maria'];
-        for(let i = 0; i < maria.length;i++){
-            const monte = maria[i];
+        lista.innerHTML = "";
+        for(let i = 0; i < pessoa.length;i++){
+            const monte = pessoa[i];
             const eLi = document.createElement('li');
             eLi.innerText = `${monte.tipo}: ${monte.qtd}`;
-            olMaria.append(eLi);
-        }
+            lista.append(eLi);
+        }   
     }
+
 }
